0d7f30bba8fa0e46548ccbd4ebebb289f0322b09
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-03 18:46:18 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -9,37 +9,51 @@ import {
     Image,
     Alert
 } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { getUsersByEmail, addUsers } from '../services/Users';
 
 export default class Login extends Component {
+
     constructor(props) {
-        super(props);
+        super(props)
     }
-    static navigationOptions = {
-        headerTitle: 'Login',
-        color: "#6200EE",
-    };
+
     state = {
-        email: 'admin',
-        password: 'admin'
+        email: '',
+        password: ''
+    }
+
+    re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    onLoginButton = () => {
+        if (re.test(this.state.email) && this.state.email == this.state.password) {
+            this.props.navigation.navigate('Home', {});
+            this.setState({
+                email: '',
+                password: ''
+            });
+
+        } else {
+            alert('Username/Password are incorrect.');
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <View style={{
-                    flexDirection: 'column'
-                }}>
-                    <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]}>
-                        <Text style={styles.loginText}>SIGN IN</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]}>
-                        <Text style={styles.loginText}>SIGN UP</Text>
-                    </TouchableHighlight>
+                <View style={{flexDirection:'row'}}>
+                    <TouchableOpacity style={[ styles.loginButton1 ]}>
+                        <Text style={[styles.login]}>SIGN IN</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[ styles.loginButton2 ]}>
+                        <Text style={[styles.login]}>SIGN UP</Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.inputContainer}>
                     <TextInput style={styles.inputs}
                         placeholder="Email"
                         keyboardType="email-address"
+                        value={this.state.email}
                         underlineColorAndroid='transparent'
                         onChangeText={(email) => this.setState({ email })} />
                 </View>
@@ -48,6 +62,7 @@ export default class Login extends Component {
                     <TextInput style={styles.inputs}
                         placeholder="Password"
                         secureTextEntry={true}
+                        value = {this.state.password}
                         underlineColorAndroid='transparent'
                         onChangeText={(password) => this.setState({ password })} />
                 </View>
@@ -75,11 +90,6 @@ const styles = StyleSheet.create({
     continue: {
         marginTop: 200,
     },
-    text:{
-        padding: 20,
-        fontSize: 30,
-        fontWeight: 'bold',
-    },
     inputContainer: {
         width: 250,
         height: 45,
@@ -88,27 +98,13 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     inputs: {
+        marginTop: 29,
         height: 45,
         borderBottomColor: 'grey',
         borderBottomWidth: 1,
         flex: 1,
         color: 'black',
         fontSize: 20,
-    },
-    inputIcon: {
-        width: 30,
-        height: 30,
-        marginLeft: 15,
-        justifyContent: 'center'
-    },
-    buttonContainer: {
-        height: 45,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        width: 100,
-        borderRadius: 30,
     },
     continueContainer: {
         height: 50,
@@ -117,8 +113,40 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    loginButton: {
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
+        justifyContent: 'center'
+    },
+    continueContainer: {
+        marginTop: 70,
+        height: 50,
+        width: 300,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: "#6200EE",
+    },
+    loginButton1: {
+        backgroundColor: "#6200EE",
+        borderRadius: 60,
+        padding: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginRight: 25,
+    },
+    loginButton2: {
+        borderRadius: 60,
+        padding: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginLeft: 25,
+    },
+    login: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '600',
     },
     loginText: {
         color: 'white',
