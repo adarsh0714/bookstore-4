87dd2ef5001957c577f1dd0b7aa8d7f7dffb34b1
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-03 14:19:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './components/Login';
 
 const Stack = createStackNavigator();
 
