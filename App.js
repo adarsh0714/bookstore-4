82e72aca1719d2ae862417b49d0bdafeb90188d1
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-03 14:17:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/Login';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="SIGN IN" component={Signin} />
+      <Stack.Screen name="SIGN UP" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
