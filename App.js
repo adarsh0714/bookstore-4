c7fcc15721f016385ad50c5f9344000070dfbc52
Preetham134 <63408868+Preetham134@users.noreply.github.com>
2021-09-03 23:20:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import Sign from './components/Start';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="SIGN IN" component={Login} />
-      <Stack.Screen name="SIGN UP" component={Login} />
+      <Stack.Navigator initialRouteName = "Sign">
+      <Stack.Screen name="Sign" component={Sign} />
+      <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
