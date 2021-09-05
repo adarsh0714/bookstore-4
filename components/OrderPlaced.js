75c5ad82c80254c9b24e44c9bd2610cca21425a7
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-05 13:44:30 +0530

    Add files via upload

---
+++ components/OrderPlaced.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View ,Button, TouchableHighlight} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrderPlaced () {
+    const navigation = useNavigation();
+
+    const placeorder = () => {
+        navigation.navigate('');   //next page link here
+    }
+
+    return(
+        <TouchableOpacity >
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <Image
+                    style={styles.tinyLogo}
+                    source={{
+                    uri: "https://cdn1.iconfinder.com/data/icons/user-interface-colorful/48/check_circle-512.png" ,
+                    }}
+                />   
+            </View>
+            <Text style={styles.title}>Order Placed! </Text>
+            <Text style={styles.text}>Your order was placed successfully.</Text>
+            <TouchableHighlight style={[ styles.ordersButton ]} onPress={() => placeorder(navigation)}>
+                    <Text style={styles.ordersText}> MY ORDERS</Text>
+            </TouchableHighlight>
+        </TouchableOpacity>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 20,
+    },
+    title: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 32,
+        marginLeft: 100,
+        marginTop: 213,
+        color: '#555555',
+        fontWeight: '600'
+    },
+    text: {
+      fontSize: 20,
+      marginLeft:40,
+      marginTop: 18,
+      color: '#555555',
+      fontWeight: '500'
+    },
+    tinyLogo: {
+      width: 98,
+      height: 98,
+      marginTop:260,
+    },
+    ordersButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width:150,
+        borderRadius:50,
+        backgroundColor: "#6200ee",
+        fontWeight:"800",
+        color:'white',
+        marginTop: 100,
+        marginLeft:120,
+    },
+    ordersText: {
+        color: 'white',
+        fontWeight:"800"
+    },
+});
