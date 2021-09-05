d1eb6989d1cd517e074bf91f0dcec8fa2632f734
Deepakyara <89625297+Deepakyara@users.noreply.github.com>
2021-09-05 16:53:35 +0530

    Update CartData.js

--- services/CartData.js
+++ services/CartData.js
@@ -1,13 +1,13 @@
 
 let books =[
-    {id:1,by:'Katie Daynes',buy:'699',year:'2019',title:"Book 1",subtitle:"What is a virus?",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474991513_cover_image.jpg"},
-    {id:2,by:'Swathi Roy',buy:'425',year:'2018',title:"Book 2",subtitle:"Ready for Writing",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474986694_cover_image.jpg"},
-    {id:3,by:'Yashwanti S',buy:'574',year:'2001',title:"Book 3",subtitle:"Springtime",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474985420_cover_image.jpg"},
-    {id:4,by:'Kapish K',buy:'860',year:'2015',title:"Book 4",subtitle:"Billy Mini Monsters",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474978439_cover_image.jpg"},
-    {id:5,by:'Ishika A',buy:'740',year:'2014',title:"Book 5",subtitle:"The Infinity Files",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474972208_cover_image.jpg"},
-    {id:6,by:'Abhhash J',buy:'240',year:'2005',title:"Book 6",subtitle:"The Unhurry Book",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474970495_cover_image.jpg"},
-    {id:7,by:'Pranay Jain',buy:'600',year:'2012',title:"Book 7",subtitle:"The Sleeping Prince",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474969802_cover_image.jpg"},
-    {id:8,by:'Rahul B',buy:'499',year:'2007',title:"Book 8",subtitle:"The Shark Caller",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474966849_cover_image.jpg"}
+    {id:1,by:'Katie Daynes',buy:'19',year:'2019',title:"Book 1",subtitle:"What is a virus?",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474991513_cover_image.jpg"},
+    {id:2,by:'Swathi Roy',buy:'12',year:'2018',title:"Book 2",subtitle:"Ready for Writing",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474986694_cover_image.jpg"},
+    {id:3,by:'Yashwanti S',buy:'17',year:'2001',title:"Book 3",subtitle:"Springtime",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474985420_cover_image.jpg"},
+    {id:4,by:'Kapish K',buy:'26',year:'2015',title:"Book 4",subtitle:"Billy Mini Monsters",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474978439_cover_image.jpg"},
+    {id:5,by:'Ishika A',buy:'14',year:'2014',title:"Book 5",subtitle:"The Infinity Files",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474972208_cover_image.jpg"},
+    {id:6,by:'Abhhash J',buy:'24',year:'2005',title:"Book 6",subtitle:"The Unhurry Book",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474970495_cover_image.jpg"},
+    {id:7,by:'Pranay Jain',buy:'20',year:'2012',title:"Book 7",subtitle:"Sleeping Prince",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474969802_cover_image.jpg"},
+    {id:8,by:'Rahul B',buy:'19',year:'2007',title:"Book 8",subtitle:"The Shark Caller",link:"https://training.pyther.com/yara/15-day/03-BookStore/books/9781474966849_cover_image.jpg"}
 ];
 
 let cart=[
@@ -35,9 +35,9 @@ export var doAdd = (data)=>{
         }
     } 
 }
-export var incrementCount = (data)=>{
+export var incrementCount = (id)=>{
     for(let i=0;i<cart.length;i++){
-        if (cart[i].bookID==data){
+        if (cart[i].bookID==id){
             cart[i].count++;
         }
     }
@@ -46,7 +46,13 @@ export var incrementCount = (data)=>{
 export var decrementCount = (data)=>{
     for(let i=0;i<cart.length;i++){
         if (cart[i].bookID==data){
-            cart[i].count--;
+            if(cart[i].count==1)
+            {
+                cart=cart.filter(item=>item.bookID!=data)
+            }
+            else{
+                cart[i].count--;
+            }
         }
     }    
 }
