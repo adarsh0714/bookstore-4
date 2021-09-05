b1be78e04dc19963b9df2c1c8757ae7c78455e43
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-05 23:29:22 +0530

    Delete services directory

--- services/Users.js
+++
@@ -1,9 +0,0 @@
-var users = [
-    {email: 'abc@abc.com', password: 'abc'},
-    {email: 'gef@abc.com', password: 'gef'},
-    {email: 'xyz@abc.com', password: 'xyz'}
-]
-
-export var addUsers = (user)=>{
-    users.push(user);
-}
